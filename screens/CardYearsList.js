cf41671367bb6b004b24aa6b8387622310dcbd25
Karolina <dubajkaro@gmail.com>
2024-03-21 21:56:48 -0300

    add a list of years

--- screens/CardYearsList.js
+++ screens/CardYearsList.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { View, ScrollView, StyleSheet, Image } from 'react-native';
-import { Card, Button, Icon, Avatar } from '@rneui/themed';
+import { Card, Button, Icon, Avatar, Text } from '@rneui/themed';
 
 const CardYearsList = ({ navigation }) => {
+
+const currentYear = new Date().getFullYear();
+const startYear = 1901;
+const amountOfYears = currentYear - startYear;
+let loop= [];
+for (let i = currentYear; i >= startYear; i--) {
+    loop.push(
+        <Card key={i}>
+        <Card.Title  subtitle="Card Subtitle">{i}</Card.Title>
+        {i ===  new Date().getFullYear() && <Text style={styles.checkBack}>Laurates not announced yet</Text>}
+        <Card.Divider />
+</Card>
+
+    )
+    console.log(loop)
+}
+
     return (
         <ScrollView>
         <View>
-          <Card>
-            <Card.Title>Test</Card.Title>
-            <Card.Divider />
-          </Card>
+         {loop}
         </View>
       </ScrollView>
     )
 };
+
+const styles = StyleSheet.create({
+    checkBack: {
+        color: 'blue',
+        textAlign: 'center'
+    }
+});
 
 export default CardYearsList;
