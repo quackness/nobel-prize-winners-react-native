2f72770c1fda5b997db974f20886f137edef97d6
Karolina <dubajkaro@gmail.com>
2024-03-21 22:37:51 -0300

    fetch api

--- screens/PrizeDetails.js
+++ screens/PrizeDetails.js
@@ -1,11 +1,31 @@
 import React from'react';
 import { View} from 'react-native';
 import {  Text } from '@rneui/themed';
+import { useEffect , useState} from 'react';
 
 const PrizeDetails = ({ navigation }) => {
+    const [laureates, setLaurates] = useState([]);
+
+    const baseUrl = 'https://api.nobelprize.org/2.1/laureates?_ga=2.190918061.602221117.1710982068-1902647732.1710982068';
+    useEffect(() => {
+        fetchLaurates();
+    }, []);
+
+    const fetchLaurates = async() => {
+        try {
+            const response = await fetch(baseUrl);
+            const data = await response.json();
+            setLaurates(data.laureates);
+        } catch {
+            console.error('Error:', error);
+        }
+   
+    }
     return (
         <View>
-            <Text>Prize Details</Text>
+            {laureates.map((laureate, index) => {
+            return <Text key={index}>{laureate.knownName.en}</Text>
+})}
         </View>
     )
 };
