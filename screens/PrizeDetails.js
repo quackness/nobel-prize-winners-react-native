f8f35aec2c48cab9bb4d195426fcd44a837363aa
Karolina <dubajkaro@gmail.com>
2024-03-23 17:03:10 -0300

    output the names

--- screens/PrizeDetails.js
+++ screens/PrizeDetails.js
@@ -1,16 +1,21 @@
 import React from'react';
 import { View} from 'react-native';
-import {  Text } from '@rneui/themed';
+import {  Text, ListItem } from '@rneui/themed';
 import { useEffect , useState} from 'react';
 
 const PrizeDetails = ({ navigation, route}) => {
     const [laureates, setLaurates] = useState([]);
     console.log(route.params.category);
     console.log(route.params.year);
+    const category = route.params.category
+    const catCode = category.slice(0,3).toLowerCase();
+    console.log(catCode);
 
   
 
-    const baseUrl = `http://api.nobelprize.org/v1/prize.json`;
+    const baseUrl = `https://api.nobelprize.org/2.1/nobelPrize/${catCode}/${route.params.year}`;
+
+
     useEffect(() => {
         fetchLaurates();
     }, []);
@@ -19,20 +24,28 @@ const PrizeDetails = ({ navigation, route}) => {
         try {
             const response = await fetch(baseUrl);
             const data = await response.json();
-            console.log(data.prizes)
-            setLaurates(data.prizes);
-        } catch {
+   
+            setLaurates(data[0].laureates);
+        } catch(error) {
             console.error('Error:', error);
         }
    
     }
+
+    console.log(laureates);
     return (
         <View>
-            {/* {laureates.map((laureate, index) => {
-            return <Text key={index}>{laureate.awardYear}</Text>
-})} */}
-<Text>{route.params.category}</Text>
-        </View>
+        <Text>{route.params.category}</Text>
+        <Text>{route.params.year}</Text>
+        <Text>Laurates</Text>
+        {laureates.map((laureate, index) => (
+            <ListItem key={index}>
+                <ListItem.Content>
+                    <ListItem.Title>{laureate.knownName.en}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+        ))}
+    </View>
     )
 };
 export default PrizeDetails;
